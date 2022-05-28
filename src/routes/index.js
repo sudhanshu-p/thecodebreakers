e3e9327b413c1ad45bd7a56c0172cce8022585d4
Samruddhitatiwar <87266475+Samruddhitatiwar@users.noreply.github.com>
2022-05-28 12:38:02 +0530

    event page commits

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "/public/styles.css";
+import Header from "./event-components/Header";
+import Card from "./event-components/Card";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Card />
+    </div>
+  );
+}
+export default App;
