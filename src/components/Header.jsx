f108c4540a941db8c32d0e4e1b385a7708492e0e
Samruddhitatiwar <87266475+Samruddhitatiwar@users.noreply.github.com>
2022-05-28 12:32:01 +0530

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Header() {
+  return (
+    <div>
+      <h1 className="heading">College Vs Professional Life</h1>
+      <h2 className="heading2">Learning how to learn alone!</h2>
+    </div>
+  );
+}
+export default Header;
