61a23c0cf8814791f7943bd486ac04f1d23fc4b8
Khushi Shroff <86292101+Ks103@users.noreply.github.com>
2022-05-28 10:55:58 +0530

    initial commit

---
+++ src/components/header.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function Header(props) {
+    return (
+       
+            <h1>Register Here</h1>
+            
+        
+    );
+}
+
+export default Header;
