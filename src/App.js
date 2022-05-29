85ee024dfc383635b4f002df9363077879210d75
henk1729 <80308332+henk1729@users.noreply.github.com>
2022-05-29 15:03:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Head from "./Header.js";
+import Card from "./Card.js";
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Routes/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Head from "../Components/Header.js";
+import Card from "../Components/Card";
+
+function App() {
+  return (
+    <div className="App">
+      <Head />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
