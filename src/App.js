b28fc44a2d0aeea1af0b76bef7bd0fa1b336f8e2
henk1729 <80308332+henk1729@users.noreply.github.com>
2022-06-06 00:03:01 +0530

    Achievements Section (#4)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './routes/Home';
+import Register from './routes/Register';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Home /> */}
+      <Register />
     </div>
   );
 }
