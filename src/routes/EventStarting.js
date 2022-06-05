5efd2f3be85b482a8ff090a2d3dd30ebea329c2c
AdityaBopche25 <92738584+AdityaBopche25@users.noreply.github.com>
2022-06-05 21:39:50 +0530

    Main Event Page (#3)

---
+++ src/routes/EventStarting.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+import Newevent from './Components/Newevent';
+import Section from './Components/Section';
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Newevent/>
+      <Section/>
+    </>
+  );
+}
+
+export default App;
